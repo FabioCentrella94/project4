b9c530aa8335f3034b85917799bd2fca484e445e
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-01-18 18:06:53 +0000

    fdkjafkda

---
+++ js/js.js
@@ -0,0 +1,8 @@
+let itemsForSale = document.getElementsByTagName('img');
+let basket = document.getElementsByClassName('list__basket');
+
+itemsForSale.addEventListener('click', () => {
+    basket = [];
+    basket.push();
+})
+
