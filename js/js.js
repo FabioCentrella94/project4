ad416d184afaf53d8ece1daf5815cf0b11db8ca7
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-01-19 15:15:58 +0000

    emptyed branch

--- js/js.js
+++
@@ -1,8 +0,0 @@
-let itemsForSale = document.getElementsByTagName('img');
-let basket = document.getElementsByClassName('list__basket');
-
-itemsForSale.addEventListener('click', () => {
-    basket = [];
-    basket.push();
-})
-
